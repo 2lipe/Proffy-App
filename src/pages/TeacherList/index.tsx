eb7e06c14302fcd059f36d636fd5a6793ed4fddd
Felipe <felipe.svfx@gmail.com>
2020-08-05 12:28:50 -0300

    Adicionado componente de input na pagina de listagem

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,28 +2,19 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
 
 import './styles.css';
+
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis." >
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Materia</label>
-            <input type="text" id="subject"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="week-day">Dia da semana</label>
-            <input type="text" id="week-day"/>
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <Input name="subject" label="Matéria" />
+          <Input name="week-day" label="Dia da semana" />
+          <Input type="time" name="time" label="Hora" />
         </form>
       </PageHeader>
 
