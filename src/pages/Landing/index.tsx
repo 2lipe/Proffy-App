3e184a702c20a613c8501d40b6087fda1ac86886
Felipe <felipe.svfx@gmail.com>
2020-08-05 15:51:02 -0300

    Adicionado funcao para cadastro de aulas

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -11,6 +13,16 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(res => {
+      const  { total } = res.data;
+
+      setTotalConnections(total);
+    })
+  }, [])
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total de 200 conexões já realizadas 
+          Total de {totalConnections} conexões já realizadas 
           <img src={purpleHeartIcon} alt="Coração roxo"/>
         </span>
 
