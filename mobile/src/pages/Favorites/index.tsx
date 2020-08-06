5952fb5cd8e424b171386810e2b7c648151037e9
Felipe <felipe.svfx@gmail.com>
2020-08-06 15:38:31 -0300

    Adiciando cabecalho nas paginas de listagem e favoritos

--- mobile/src/pages/Favorites/index.tsx
+++ mobile/src/pages/Favorites/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function Favorites() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} >
+      <PageHeader title="Meus Proffys favoritos" />
     </View>
   );
 }
--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View } from 'react-native';
 
+import PageHeader from '../../components/PageHeader';
+
 import styles from './styles';
 
 function TeacherList() {
   return (
     <View style={styles.container} >
+      <PageHeader title="Proffys disponíveis" />
     </View>
   )
 }
